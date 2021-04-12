bb75bb1b1393e01cf64e2d39f2b53310bc681fff
Aaron Thompson <athompson812@hotmail.com>
2021-04-11 19:05:53 -0700

    Created basic frontend layout in React

---
+++ frontend/react-app/src/App.js
@@ -0,0 +1,14 @@
+import Layout from './components/Layout';
+import Urls from './Urls'
+
+function App() {
+  return (
+    <div className="App">
+      <Layout>
+        <Urls />
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
