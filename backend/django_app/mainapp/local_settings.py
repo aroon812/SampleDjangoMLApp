6143195fb120562521a9d7d6692e3df873255fe7
Aaron Thompson <athompson812@hotmail.com>
2021-04-29 19:09:07 -0700

    Added Redux action types to the frontend.

--- backend/django_app/mainapp/local_settings.py
+++ backend/django_app/mainapp/local_settings.py
@@ -2,7 +2,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'predictiondb',
-        'USER': 'postgres',
+        'USER': 'aaronthompson',
         #'PASSWORD': 'postgres_password',
         'HOST': '127.0.0.1',
         'PORT': '5432',
